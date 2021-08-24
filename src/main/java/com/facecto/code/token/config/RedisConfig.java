01da488708fa14ab1ddb72d790f5a85c3e074fca
Jon <36321525@qq.com>
2021-08-24 11:01:24 +0800

    fix redis bug

---
+++ src/main/java/com/facecto/code/token/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.facecto.code.token.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author Jon So, https://cto.pub, https://github.com/facecto
+ * @version v1.1.0 (2021/08/08)
+ */
+@EnableCaching
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setValueSerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        return redisTemplate;
+    }
+}
